5ee916fb8ca22bdaab3127c2c30b9d37650e1b4d
L-chang-an <80374277+L-chang-an@users.noreply.github.com>
2022-06-29 16:29:12 +0800

    Update testjs.js

--- testjs.js
+++ testjs.js
@@ -1,18 +1,6 @@
-msg = "javascript"
-document.write(msg)
 
-// let changeBut= document.querySelector("button");
 let changeHeader=document.querySelector("h1");
-// let head1=changeHeader.querySelector("")
-// changeBut.onclick=function (){
-//     let butId=changeBut.id;
-//     if (butId==="change-red"){
-//         changeHeader.setAttribute("color","red");
-//     }
-//     else{
-//         changeHeader.setAttribute("color","aqua");
-//     }
-// }
+
 
 function changeToRed() {
     changeHeader.textContent="hello"
